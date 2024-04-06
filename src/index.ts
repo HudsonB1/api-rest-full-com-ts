91ad2c8b78e96a865f5af36798e323c629a7ad91
Hudson Baroni <hudsonbarni@gmail.com>
2024-04-06 17:24:15 -0300

    integrando express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import express from "express";
+import config from "config";
+
+const app = express();
+
+//JSON middleware
+app.use(express.json());
+
+// app port
+const port = config.get<number>("port");
+
+app.listen(9696, async () => {
+   console.log(`Server started on port ${9696}!`)
+});
